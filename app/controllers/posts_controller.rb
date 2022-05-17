b7b3b440d041b34285568864e1b79d0010be95bf
verissimo francis <verissimotoyin50@gmail.com>
2022-05-17 19:18:07 +0100

    Fix Linter Errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
-  def index;
+  def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
-  def show;
+  def show
     @post = Post.find(params[:id])
   end
 end
