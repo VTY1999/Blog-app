d20c9e2bfefd694787ebcc24f2a2b54977e45e07
verissimo francis <verissimotoyin50@gmail.com>
2022-05-18 18:03:42 +0100

    Update to markup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   after_save :posts_count
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
