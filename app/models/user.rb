3860bcbc8ea5d62881da397f12cf6870e30f66c8
verissimo francis <verissimotoyin50@gmail.com>
2022-05-24 09:21:24 +0100

    Update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  validates_presence_of :name
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  validates_presence_of :name, presence: true
   validates :posts_counter, presence: true,
                             numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  has_many :posts, foreign_key: 'user_id', dependent: :destroy
-  has_many :comments, foreign_key: 'user_id', dependent: :destroy
-  has_many :likes, foreign_key: 'user_id', dependent: :destroy
+
+  validates :email, confirmation: true
+  has_many :posts, foreign_key: 'user_id'
+  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'user_id'
 
   def latest_three_post
     posts.order(created_at: :desc).limit(3)
